933fc0a60c7aeec5e37fa614e020a3e85b2f3e5b
sikha22 <84958102+sikha22@users.noreply.github.com>
2022-07-03 21:57:57 +0530

    Add files via upload

---
+++ c++/arithmetic.cpp
@@ -0,0 +1,28 @@
+//importing libraries
+
+#include<iostream>
+using namespace std;
+int main(void){
+
+    //declare variable
+  int mySec1=7;
+  int mySec2=100;
+
+  //addition
+  int sum = mySec1+mySec2;
+ //product
+  int product = mySec1*mySec2;
+  //remainder 
+  int remainder = mySec1%mySec2;
+  //difference 
+  int diff = mySec1-mySec2;
+
+  cout<< " sum is "<<sum<<endl;
+   cout<< " product is "<<product<<endl;
+    cout<< " remainder is "<<remainder<<endl; 
+    cout<< " difference is "<<diff<<endl;
+
+
+
+    
+}
