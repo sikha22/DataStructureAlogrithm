563fbdabd84cfa1d846ac1766ba75f6838f93a26
sikha22 <84958102+sikha22@users.noreply.github.com>
2022-07-03 22:11:51 +0530

    Add files via upload

---
+++ c++/compare.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(void){
+     int a,b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<" Enter b:";
+    cin>>b;
+    if(a>b){
+        cout<<"Hello"<<endl;
+    }
+    else if(a==b)   {
+
+           cout<<"Equal"<<endl;
+       }
+       else{
+           cout<<"Lesser"<<endl;
+       }
+      
+         
+      return 0;
+}
