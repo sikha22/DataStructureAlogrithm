701acc8cae744296dd987858fe5572b1f94fbc6a
sikha22 <84958102+sikha22@users.noreply.github.com>
2022-07-03 22:25:24 +0530

    Add files via upload

---
+++ c++/conditional.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int a,b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<" Enter b:";
+    cin>>b;
+    if(a>b){
+        cout<<"Hello";
+    }
+    else{
+        cout<<"Bye";    }
+     return 0;
+}
